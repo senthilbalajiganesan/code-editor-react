a017f6e098e29d80ba450ad8be576d9068d22227
SenthilBalaji <senthil.g.balaji@gmail.com>
2020-03-07 19:44:43 +0530

    added port changes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const compression = require("compression");
 const app = express();
 app.use(compression());
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, "/build")));
 
 // Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-app.listen(3502);
+app.listen(process.env.PORT || 8080);
